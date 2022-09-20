5a95a4710e21d6b41e243fe4c0bd5c98efd0bd76
Senayd <doganaysenay@hotmail.com>
2022-09-20 22:57:16 +0300

    Kalin ses mi değil mi ?

---
+++ javademos/src/javademos/miniProje2Sesler.java
@@ -0,0 +1,23 @@
+package javademos;
+
+public class miniProje2Sesler {
+
+	public static void main(String[] args) {
+
+		char ses = 'U';
+
+		switch (ses) {
+		case 'A':
+		case 'I':
+		case 'O':
+		case 'U':
+			System.out.println("Kalın sesli harftir");
+
+			break;
+		default:
+			System.out.println("Ince sesli harftir");
+
+		}
+	}
+
+}
