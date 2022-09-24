3ef251d34f3571c1be0a1aa2e128fd6ec32b7823
Senayd <doganaysenay@hotmail.com>
2022-09-24 23:48:19 +0300

    overloading

---
+++ javademos/src/methodsOverloading/Main.java
@@ -0,0 +1,14 @@
+package methodsOverloading;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		DortIslem dortIslem = new DortIslem();
+
+		System.out.println(dortIslem.topla(3,50));
+		System.out.println(dortIslem.topla(3,50,15));
+
+	}
+
+}
